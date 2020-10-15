d946471fb84ac17395e4d6228501387d55177764
xeusteerapat <xeus085@gmail.com>
2020-10-15 10:35:23 +0700

    [update] fetch data from react table

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Chrono } from 'react-chrono';
+import axios from 'axios';
 
 const mockData = [
   {
@@ -25,13 +26,26 @@ const mockData = [
 ];
 
 const Timeline = () => {
+  const [timelineData, setTimelineData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`https://api.airtable.com/v0/${process.env.REACT_APP_AIR_TABLE_BASE_ID}/${process.env.REACT_APP_AIR_TABLE_NAME}?api_key=${process.env.REACT_APP_AIR_TABLE_API_KEY}
+`);
+      const transformData = response.data.records.map(item => item.fields);
+      setTimelineData(transformData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <h1>
+    <>
       <h1>Timeline</h1>
       <div style={{ width: '500px', height: '950px' }}>
         <Chrono items={mockData} mode='TREE' />
       </div>
-    </h1>
+    </>
   );
 };
 
