db63ca33572f322d738fde6cf06be2c120c54aa1
xeusteerapat <xeus085@gmail.com>
2020-10-29 10:45:32 +0700

    [update] change data source from mock data to airtable

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,29 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Chrono } from 'react-chrono';
 import axios from 'axios';
 
-const mockData = [
-  {
-    title: '8 Sep 2020',
-    contentTitle: 'Micro Learning',
-    contentText: 'Start develop Jump Dashboard',
-  },
-  {
-    title: '21 Sep 2020',
-    contentTitle: 'Micro Learning',
-    contentText: 'Start develop Chatbot Manager',
-  },
-  {
-    title: '28 Sep 2020',
-    contentTitle: 'Chatbot',
-    contentText: 'Start develop Flash',
-  },
-  {
-    title: '16 Oct 2020',
-    contentTitle: 'Micro Learning',
-    contentText: 'Complete Jump Dashboard',
-  },
-];
-
 const Timeline = () => {
   const [timelineData, setTimelineData] = useState([]);
 
@@ -39,12 +16,18 @@ const Timeline = () => {
     fetchData();
   }, []);
 
+  const fetchTimeline = () => {
+    if (timelineData.length === 0) {
+      return <div>Loading...</div>;
+    }
+
+    return <Chrono items={timelineData} mode='TREE' />;
+  };
+
   return (
     <>
       <h1>Timeline</h1>
-      <div style={{ width: '500px', height: '950px' }}>
-        <Chrono items={mockData} mode='TREE' />
-      </div>
+      <div style={{ width: '500px', height: '950px' }}>{fetchTimeline()}</div>
     </>
   );
 };
